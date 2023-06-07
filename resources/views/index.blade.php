6216f71ba7e76723c24a96d4b660ea7fc4facc02
shakeelahmad <shakeelbajeeda@gmail.com>
2023-06-07 15:50:19 +0500

    working on frontend integrationn

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,130 +1,23 @@
 @extends('layouts.app')
 @section('content')
-    <!-- Slideshow with video background begin-->
-    <div class="container-fluid slideshow">
-        <div class="video-background">
-            <video autoplay muted loop>
-                <source src="video/windsource.mp4" type="video/mp4">
-            </video>
-            <div class="slide-content">
-                <h1>Tassie Green Energy Trading Company (TaGET)</h1>
-                <p>Tasmanias trusted energy trading company</p>
-                <div class="card bg-white p-3">
-                    <form class="form-inline my-2 my-lg-0" action="{{ route('trading') }}" method="get">
-                        <input class="form-control mr-sm-2 col-md-8" type="text" name="search" placeholder="Search by type or zone"
-                               aria-label="Search">
-                        <button class="btn btn-outline-success my-2 my-sm-0 col-md-3" type="submit">Search</button>
-                    </form>
-                </div>
+    <h1 class="title">Renewable Energy Trading System</h1>
+    <p><center><img src="{{asset('website/Images/TGET.jpg')}}" width="1000" height ="300"/><br></center></p>
+    <div class="container mt-3">
+        <h2>Search Renewable Energy</h2>
+        <form>
+            <div class="form-group">
+                <label for="energy-type">Energy Type:</label>
+                <select class="form-control" id="energy-type">
+                    <option>Wind</option>
+                    <option>Solar</option>
+                    <option>Hydro</option>
+                </select>
             </div>
-        </div>
-    </div>
-    <hr>
-    <!-- Slideshow with video background end-->
-    <!--about section start -->
-    <div class="about_section layout_padding">
-        <div class="container">
-            <h1 class="text">Our Work</h1>
-            <div class="about_section_2">
-                <div class="row">
-                    <div class="col-lg-4">
-                        <div class="about_taital_main">
-
-                            <p class="lorem_text"> Solar panels installed on a farm in Tasmania, Australia.
-                                An important part of our work is building knowledge that can be shared openly to help
-                                people navigate the energy transition better.
-                                We are committed to long-term sustainability.Get a solar fit in no time and without
-                                risk. We believe that maximizing the use of sustainable energy will be of great help in
-                                the times to come.
-                        </div>
-                    </div>
-                    <div class="col-lg-8">
-                        <div class="about_image">
-                            <img src="image/wind-energy.jpeg">
-                        </div>
-                    </div>
-
-                </div>
+            <div class="form-group">
+                <label for="location">Zone:</label>
+                <input type="text" class="form-control" id="location" placeholder="Enter location">
             </div>
-        </div>
-        <hr>
+            <button type="submit" class="btn btn-primary">Search</button>
+        </form>
     </div>
-    <div class="about_section layout_padding">
-        <div class="container">
-            <h1 class="text">Our Work</h1>
-            <div class="about_section_2">
-                <div class="row">
-                    <div class="col-lg-8">
-                        <div class="about_image">
-                            <img src="image/solar-panel.jpeg">
-                        </div>
-                    </div>
-                    <div class="col-lg-4">
-                        <div class="about_taital_main">
-
-                            <p class="lorem_text"> TaGET is one of the world's leading wind turbine trading company with
-                                over 30,000 installed units producing wind power worldwide. Our wind energy solutions
-                                portfolio includes a range of onshore and offshore turbines with flexible support
-                                services.
-                                Need help with financing your wind power solution? Always wanting to take advantage of
-                                the best new wind power technology? We know wind turbine blades. Capturing the wind at
-                                any speed around the world on land or at sea requires the reliability of wind turbine
-                                blades.
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!--about section end -->
-    <!--about section end -->
-    <!-- services section start -->
-    <div class="services_section layout_padding">
-        <div class="container">
-            <h1 class="services_taital">How We Do</h1>
-            <p class="about_text">We provides you the best experience to provide and sell different renewable energy
-                solor and wind.</p>
-            <div class="services_section_2">
-                <div class="row">
-                    <div class="col-lg-4">
-                        <div class="icon_box">
-                            <div class="icon_1"><img src="images/icon-1.png"></div>
-                        </div>
-                        <h4 class="selection_text">Site Visit</h4>
-                        <p class="ipsum_text"></p>
-                        <div class="icon_box">
-                            <div class="icon_1"><img src="images/icon-4.png"></div>
-                        </div>
-                        <h4 class="selection_text">Project Report</h4>
-                        <p class="ipsum_text"></p>
-                    </div>
-                    <div class="col-lg-4">
-                        <div class="icon_box">
-                            <div class="icon_1"><img class="img" src="images/icon-2.png"></div>
-                        </div>
-                        <h4 class="selection_text">Research and Analytics</h4>
-                        <p class="ipsum_text"></p>
-                        <div class="icon_box">
-                            <div class="icon_1"><img src="images/icon-5.png"></div>
-                        </div>
-                        <h4 class="selection_text">Advisory Activities</h4>
-                        <p class="ipsum_text"></p>
-                    </div>
-                    <div class="col-lg-4">
-                        <div class="icon_box">
-                            <div class="icon_1"><img src="images/icon-3.png"></div>
-                        </div>
-                        <h4 class="selection_text">Designing</h4>
-                        <p class="ipsum_text"></p>
-                        <div class="icon_box">
-                            <div class="icon_1"><img src="images/icon-6.png"></div>
-                        </div>
-                        <h4 class="selection_text">Execute Project</h4>
-                        <p class="ipsum_text"></p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- services section end -->
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,120 +1,135 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- CSRF Token -->
+    <title>Renewable Energy Trading System</title>
+    <meta charset="UTF-8">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>TaGET</title>
-
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="{{ asset('css/style.css') }}">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="{{asset('website/CSS/style.css')}}">
 </head>
 <body>
 <!-- Navigation menu -->
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="{{ url('/') }}"><img src="{{asset('image/logo.png')}}" alt="TaGET Logo"></a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
-            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+{{--<nav class="navbar navbar-expand-lg navbar-light bg-light">--}}
+{{--    <a class="navbar-brand" href="{{ url('/') }}"><img src="{{asset('image/logo.png')}}" alt="TaGET Logo"></a>--}}
+{{--    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"--}}
+{{--            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">--}}
+{{--        <span class="navbar-toggler-icon"></span>--}}
+{{--    </button>--}}
+{{--    <div class="collapse navbar-collapse" id="navbarNav">--}}
+{{--        <ul class="navbar-nav">--}}
+{{--            <li class="nav-item">--}}
+{{--                <a class="nav-link active" href="{{ url('/') }}">--}}
+{{--                    Home--}}
+{{--                </a>--}}
+{{--            </li>--}}
+
+{{--            <li class="nav-item">--}}
+{{--                <a class="nav-link" href="{{ url('/trading') }}"><strong>Trading</strong></a>--}}
+{{--            </li>--}}
+
+{{--            <li class="nav-item">--}}
+{{--                <a class="nav-link" href="{{ url('/dashboard') }}"><strong>Dashboard</strong></a>--}}
+{{--            </li>--}}
+{{--            @if(auth()->check() && (auth()->user()->user_type == 'seller' || auth()->user()->user_type == 'service_manager'))--}}
+{{--            <li class="nav-item">--}}
+{{--                <a class="nav-link" href="{{ url('/renewable-energies') }}"><strong>Sell Energy</strong></a>--}}
+{{--            </li>--}}
+{{--            @endif--}}
+{{--            @if(auth()->check() && auth()->user()->user_type == 'service_manager')--}}
+{{--            <li class="nav-item">--}}
+{{--                <a class="nav-link" href="{{ url('/renewable-energy-type') }}"><strong>Master Trading</strong></a>--}}
+{{--            </li>--}}
+{{--            <li class="nav-item">--}}
+{{--                <a class="nav-link" href="{{ url('/users') }}"><strong>User Management</strong></a>--}}
+{{--            </li>--}}
+{{--            @endif--}}
+{{--            <li class="nav-item">--}}
+{{--                <a class="nav-link" href="{{ url('/trading-history') }}"><strong>Trading History</strong></a>--}}
+{{--            </li>--}}
+
+{{--            <li class="nav-item">--}}
+{{--                <a class="nav-link" href="{{ url('/profile') }}"><strong>Profile</strong></a>--}}
+{{--            </li>--}}
+{{--        </ul>--}}
+{{--    </div>--}}
+{{--    <!-- Login and Signup section -->--}}
+{{--    <!-- Right Side Of Navbar -->--}}
+{{--    <ul class="navbar-nav ms-auto">--}}
+
+{{--        <!-- Authentication Links -->--}}
+{{--        @guest--}}
+
+{{--            <li class="nav-item">--}}
+{{--                <a class="nav-link" href="{{ route('login') }}"><strong>Login</strong></a></li></a>--}}
+{{--            </li>--}}
+
+
+
+{{--            <li class="nav-item">--}}
+{{--                <a class="nav-link" href="{{ route('register') }}"><strong>Register</strong></a></a>--}}
+{{--            </li>--}}
+
+{{--        @else--}}
+
+{{--            <li class="nav-item dropdown">--}}
+{{--                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">--}}
+{{--                    <a class="dropdown-item" href="{{route('logout')}}"--}}
+{{--                       onclick="event.preventDefault();--}}
+{{--							document.getElementById('logout-form'.submit();">--}}
+{{--                        {{ __('Logout')}}--}}
+{{--                    </a>--}}
+{{--                    <li class="nav-item dropdown">--}}
+{{--                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"--}}
+{{--                           data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>--}}
+{{--                            {{ Auth::user()->name }}--}}
+{{--                        </a>--}}
+
+{{--                        <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">--}}
+{{--                            <a class="dropdown-item text-center" href="{{ route('logout') }}"--}}
+{{--                               onclick="event.preventDefault();--}}
+{{--                                                     document.getElementById('logout-form').submit();">--}}
+{{--                                {{ __('Logout') }}--}}
+{{--                            </a>--}}
+{{--                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">--}}
+{{--                                @csrf--}}
+{{--                            </form>--}}
+{{--                        </div>--}}
+{{--                    </li>--}}
+
+
+{{--                </div>--}}
+{{--            </li>--}}
+{{--        @endguest--}}
+{{--    </ul>--}}
+{{--    </div>--}}
+{{--</nav>--}}
+
+<nav class="navbar navbar-expand-lg bg-dark navbar-dark">
+    <a class="navbar-brand" href="#">TaGET</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav">
-            <li class="nav-item">
-                <a class="nav-link active" href="{{ url('/') }}">
-                    Home
-                </a>
-            </li>
-
-            <li class="nav-item">
-                <a class="nav-link" href="{{ url('/trading') }}"><strong>Trading</strong></a>
-            </li>
-
-            <li class="nav-item">
-                <a class="nav-link" href="{{ url('/dashboard') }}"><strong>Dashboard</strong></a>
-            </li>
-            @if(auth()->check() && (auth()->user()->user_type == 'seller' || auth()->user()->user_type == 'service_manager'))
-            <li class="nav-item">
-                <a class="nav-link" href="{{ url('/renewable-energies') }}"><strong>Sell Energy</strong></a>
-            </li>
-            @endif
-            @if(auth()->check() && auth()->user()->user_type == 'service_manager')
-            <li class="nav-item">
-                <a class="nav-link" href="{{ url('/renewable-energy-type') }}"><strong>Master Trading</strong></a>
+            <li class="nav-item active">
+                <a class="nav-link" href="index.html">Home</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="{{ url('/users') }}"><strong>User Management</strong></a>
-            </li>
-            @endif
-            <li class="nav-item">
-                <a class="nav-link" href="{{ url('/trading-history') }}"><strong>Trading History</strong></a>
-            </li>
-
-            <li class="nav-item">
-                <a class="nav-link" href="{{ url('/profile') }}"><strong>Profile</strong></a>
+                <a class="nav-link" href="registration.html">Registration</a>
             </li>
         </ul>
-    </div>
-    <!-- Login and Signup section -->
-    <!-- Right Side Of Navbar -->
-    <ul class="navbar-nav ms-auto">
-
-        <!-- Authentication Links -->
-        @guest
-
+        <ul class="navbar-nav ml-auto">
             <li class="nav-item">
-                <a class="nav-link" href="{{ route('login') }}"><strong>Login</strong></a></li></a>
+                <a class="nav-link" href="#">Login</a>
             </li>
-
-
-
             <li class="nav-item">
-                <a class="nav-link" href="{{ route('register') }}"><strong>Register</strong></a></a>
+                <a class="nav-link" href="#">Logout</a>
             </li>
-
-        @else
-
-            <li class="nav-item dropdown">
-                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                    <a class="dropdown-item" href="{{route('logout')}}"
-                       onclick="event.preventDefault();
-							document.getElementById('logout-form'.submit();">
-                        {{ __('Logout')}}
-                    </a>
-                    <li class="nav-item dropdown">
-                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
-                           data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                            {{ Auth::user()->name }}
-                        </a>
-
-                        <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item text-center" href="{{ route('logout') }}"
-                               onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                {{ __('Logout') }}
-                            </a>
-                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                @csrf
-                            </form>
-                        </div>
-                    </li>
-
-
-                </div>
-            </li>
-        @endguest
-    </ul>
+        </ul>
     </div>
 </nav>
 <main class="py-4">
